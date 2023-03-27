caafc45f8eb0c08281f1b49b6e5ca0f0f0b3e9f4
= <=>
2023-03-27 13:23:25 +0200

    ds

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -7,18 +7,19 @@
                <DecoratorItem/>
             </div>
 
-            <div class="projects-wrapper bg-danger">
+            <div class="projects-wrapper">
                 <div class="projects w-100 h-auto bg-info">
                    <Projects/> 
                 </div>
             </div>
 
-            <div class="navigator gap-2 mt-2">
+            <!-- navigator -->
+            <div class="navigator gap-2 mt-2 ">
                 <a href="#1">
-                        <li class="nav-button list-unstyled"></li>
+                    <li class="nav-button  list-unstyled"></li>
                 </a>
                 <a href="#2">
-                    <li class="nav-button list-unstyled"></li>
+                        <li class="nav-button list-unstyled"></li>
                 </a>
                 <a href="#3">
                     <li class="nav-button list-unstyled"></li>
@@ -30,6 +31,9 @@
                     <li class="nav-button list-unstyled"></li>
                 </a>
                 <a href="#6">
+                    <li class="nav-button list-unstyled"></li>
+                </a>
+                <a href="#7">
                     <li class="nav-button list-unstyled"></li>
                 </a>
             </div>
